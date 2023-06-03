67f3d42e8c2b5dc612d013b9975ce3f4daf9276d
Taurese01 <118141558+Taurese01@users.noreply.github.com>
2023-06-04 01:51:16 +0200

    uitgebreide update

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,28 @@
-// JavaScript Document
-console.log("howdy");
+var nummer1 = document.querySelector(".nummer1");
+var muziekje1 = new Audio('muziek/hurricane.m4a');
+var liedjebezig = false;
+
+function liedje1() {
+	nummer1.src = "images/video-pause-button.png";
+	muziekje1.play();
+  liedjebezig = true;
+}
+
+
+// function pauzeActie1() {
+// 	nummer1.src = "images/play-button.png";
+
+//   if (liedjebezig == true) {
+//     muziekje1.pause();
+//   } else {
+//     muziekje1.play();
+//   }
+
+
+// }
+
+
+nummer1.addEventListener("click", liedje1);
+// nummer1.addEventListener("click", pauzeActie1);
+
+
