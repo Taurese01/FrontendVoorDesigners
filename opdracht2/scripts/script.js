9e7b67eba1dd9fbb18cb724a45cd70a79cf82075
Taurese01 <118141558+Taurese01@users.noreply.github.com>
2023-06-06 14:17:43 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,5 +1,9 @@
+
+
+// Muziek
+
 var nummer1 = document.querySelector(".nummer1");
-var muziekje1 = new Audio('muziek/hurricane.m4a');
+var muziekje1 = new Audio('muziek/GodsPlan.mp3');
 var liedjebezig = false;
 
 function liedje1() {
@@ -22,7 +26,54 @@ function liedje1() {
 // }
 
 
+
 nummer1.addEventListener("click", liedje1);
 // nummer1.addEventListener("click", pauzeActie1);
 
 
+
+var sort = document.querySelector(".sort1");
+var sort2 = document.querySelector(".sort2");
+var options = {
+  valueNames: [ 'name', 'tijd' ]
+};
+
+// Stap 3.
+var charactersList = new List('theList', options);
+
+// Stap 4.
+function sortLiedjes(){
+  charactersList.sort('name', { order: "asc" });
+}
+
+
+function sortLiedjes2(){
+  charactersList.sort('tijd', { order: "asc" });
+}
+
+sort.addEventListener("click", sortLiedjes);
+sort2.addEventListener("click", sortLiedjes2);
+
+
+
+// Drag & Drop
+
+// Stap 5. --> CSS vanaf regel 58
+
+/* hopla klaar */
+
+/* getting started --> http://sortablejs.github.io/Sortable/#simple-list */
+
+// Stap 1. --> klik op het tandwiel (vergeet de https:// niet)
+
+var list = document.querySelector(".list");
+// Stap 2.
+new Sortable(list, {
+  animation: 300, // ca 300ms is meestal een mooie tijd
+});
+
+
+// Stap 3. --> nu verder met de css
+
+
+/* hopla klaar */
