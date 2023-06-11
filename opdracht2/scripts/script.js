d873a8eeed7c63c648d4ec617450efb94da746fb
Taurese01 <118141558+Taurese01@users.noreply.github.com>
2023-06-11 14:31:27 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,79 +1,458 @@
-
-
-// Muziek
+// alle liedjes van de website 
+// Regel 5 t/m 289 (code word 20 keer herhaald)
+// De function zorgt ervoor dat er word gekeken of het nummer afspeeld, als dit het geval is dan zet hij deze op pauze, en als dit niet het geval is dan word deze gestart. 
 
 var nummer1 = document.querySelector(".nummer1");
-var muziekje1 = new Audio('muziek/GodsPlan.mp3');
-var liedjebezig = false;
+var liedjebezig1 = false;
+var audio = document.querySelectorAll("Audio")
 
 function liedje1() {
-	nummer1.src = "images/video-pause-button.png";
-	muziekje1.play();
-  liedjebezig = true;
+  if (liedjebezig1) {
+    muziekje1.pause();
+    liedjebezig1 = false;
+    muziekpauze();
+
+  } else {
+    muziekje1.play();
+    liedjebezig1 = true;
+
+  }
+}
+
+var nummer2 = document.querySelector(".nummer2");
+var liedjebezig2 = false;
+
+function liedje2() {
+  if (liedjebezig2) {
+    muziekje2.pause();
+    liedjebezig2 = false;
+    muziekpauze();
+  } else {
+    muziekje2.play();
+    liedjebezig2 = true;
+
+  }
+}
+
+var nummer3 = document.querySelector(".nummer3");
+var liedjebezig3 = false;
+
+function liedje3() {
+  if (liedjebezig3) {
+    muziekje3.pause();
+    liedjebezig3 = false;
+    muziekpauze();
+  } else {
+    muziekje3.play();
+    liedjebezig3 = true;
+  }
+}
+
+var nummer4 = document.querySelector(".nummer4");
+var liedjebezig4 = false;
+
+function liedje4() {
+  if (liedjebezig4) {
+    muziekje4.pause();
+    liedjebezig4 = false;
+    muziekpauze();
+  } else {
+    muziekje4.play();
+    liedjebezig4 = true;
+  }
+}
+
+var nummer5 = document.querySelector(".nummer5");
+var liedjebezig5 = false;
+
+function liedje5() {
+  if (liedjebezig5) {
+    muziekje5.pause();
+    liedjebezig5 = false;
+    muziekpauze();
+  } else {
+    muziekje5.play();
+    liedjebezig5 = true;
+  }
+}
+
+var nummer6 = document.querySelector(".nummer6");
+var liedjebezig6 = false;
+
+function liedje6() {
+  if (liedjebezig6) {
+    muziekje6.pause();
+    liedjebezig6 = false;
+    muziekpauze();
+  } else {
+    muziekje6.play();
+    liedjebezig6 = true;
+  }
+}
+
+var nummer7 = document.querySelector(".nummer7");
+var liedjebezig7 = false;
+
+function liedje7() {
+  if (liedjebezig7) {
+    muziekje7.pause();
+    liedjebezig7 = false;
+    muziekpauze();
+  } else {
+    muziekje7.play();
+    liedjebezig7 = true;
+  }
+}
+
+var nummer8 = document.querySelector(".nummer8");
+var liedjebezig8 = false;
+
+function liedje8() {
+  if (liedjebezig8) {
+    muziekje8.pause();
+    liedjebezig8 = false;
+    muziekpauze();
+  } else {
+    muziekje8.play();
+    liedjebezig8 = true;
+  }
+}
+
+var nummer9 = document.querySelector(".nummer9");
+var liedjebezig9 = false;
+
+function liedje9() {
+  if (liedjebezig9) {
+    muziekje9.pause();
+    liedjebezig9 = false;
+    muziekpauze();
+  } else {
+    muziekje9.play();
+    liedjebezig9 = true;
+  }
+}
+
+var nummer10 = document.querySelector(".nummer10");
+var liedjebezig10 = false;
+
+function liedje10() {
+  if (liedjebezig10) {
+    muziekje10.pause();
+    liedjebezig10 = false;
+    muziekpauze();
+  } else {
+    muziekje10.play();
+    liedjebezig10 = true;
+  }
+}
+
+var nummer11 = document.querySelector(".nummer11");
+var liedjebezig11 = false;
+
+function liedje11() {
+  if (liedjebezig11) {
+    muziekje11.pause();
+    liedjebezig11 = false;
+    muziekpauze();
+  } else {
+    muziekje11.play();
+    liedjebezig11 = true;
+  }
+}
+
+var nummer12 = document.querySelector(".nummer12");
+var liedjebezig12 = false;
+
+function liedje12() {
+  if (liedjebezig12) {
+    muziekje12.pause();
+    liedjebezig12 = false;
+    muziekpauze();
+  } else {
+    muziekje12.play();
+    liedjebezig12 = true;
+  }
+}
+
+var nummer13 = document.querySelector(".nummer13");
+var liedjebezig13 = false;
+
+function liedje13() {
+  if (liedjebezig13) {
+    muziekje13.pause();
+    liedjebezig13 = false;
+    muziekpauze();
+  } else {
+    muziekje13.play();
+    liedjebezig13 = true;
+  }
 }
 
 
-// function pauzeActie1() {
-// 	nummer1.src = "images/play-button.png";
+var nummer14 = document.querySelector(".nummer14");
+var liedjebezig14 = false;
 
-//   if (liedjebezig == true) {
-//     muziekje1.pause();
-//   } else {
-//     muziekje1.play();
-//   }
+function liedje14() {
+  if (liedjebezig14) {
+    muziekje14.pause();
+    liedjebezig14 = false;
+    muziekpauze();
+  } else {
+    muziekje14.play();
+    liedjebezig14 = true;
+  }
+}
+
+var nummer15 = document.querySelector(".nummer15");
+var liedjebezig15 = false;
+
+function liedje15() {
+  if (liedjebezig15) {
+    muziekje15.pause();
+    liedjebezig15 = false;
+    muziekpauze();
+  } else {
+    muziekje15.play();
+    liedjebezig15 = true;
+  }
+}
+
+var nummer16 = document.querySelector(".nummer16");
+var liedjebezig16 = false;
+
+function liedje16() {
+  if (liedjebezig16) {
+    muziekje16.pause();
+    liedjebezig16 = false;
+    muziekpauze();
+  } else {
+    muziekje16.play();
+    liedjebezig16 = true;
+  }
+}
+
+var nummer17 = document.querySelector(".nummer17");
+var liedjebezig17 = false;
+
+function liedje17() {
+  if (liedjebezig17) {
+    muziekje17.pause();
+    liedjebezig17 = false;
+    muziekpauze();
+  } else {
+    muziekje17.play();
+    liedjebezig17 = true;
+  }
+}
+
+var nummer18 = document.querySelector(".nummer18");
+var liedjebezig18 = false;
+
+function liedje18() {
+  if (liedjebezig18) {
+    muziekje18.pause();
+    liedjebezig18 = false;
+    muziekpauze();
+  } else {
+    muziekje18.play();
+    liedjebezig18 = true;
+  }
+}
+
+var nummer19 = document.querySelector(".nummer19");
+var liedjebezig19 = false;
+
+function liedje19() {
+  if (liedjebezig19) {
+    muziekje19.pause();
+    liedjebezig19 = false;
+    muziekpauze();
+  } else {
+    muziekje19.play();
+    liedjebezig19 = true;
+  }
+}
 
 
-// }
+var nummer20 = document.querySelector(".nummer20");
+var liedjebezig20 = false;
+
+function liedje20() {
+  if (liedjebezig20) {
+    muziekje20.pause();
+    liedjebezig20 = false;
+    muziekpauze();
+  } else {
+    muziekje20.play();
+    liedjebezig20 = true;
+  }
+}
+
+
+// Stopt alle liedjes (wil hier nog een efficientere versie van maken, waarbij je niet nog op het nieuwe liedje op pauze moet drukken voordat beide liedjes stoppen)
+var pauze = document.querySelectorAll("audio");
+
+function muziekpauze() {
+  pauze.forEach(function (audio) {
+    audio.pause(); // Pauzeer elk audiofragment
+  });
+}
+
+// hulp sanne
+// curAudio.addEventListener("ended", playNextSong)
 
 
 
+// De Event listeners voor het starten van het liedje!
 nummer1.addEventListener("click", liedje1);
-// nummer1.addEventListener("click", pauzeActie1);
+nummer2.addEventListener("click", liedje2);
+nummer3.addEventListener("click", liedje3);
+nummer4.addEventListener("click", liedje4);
+nummer5.addEventListener("click", liedje5);
+nummer6.addEventListener("click", liedje6);
+nummer7.addEventListener("click", liedje7);
+nummer8.addEventListener("click", liedje8);
+nummer9.addEventListener("click", liedje9);
+nummer10.addEventListener("click", liedje10);
+nummer11.addEventListener("click", liedje11);
+nummer12.addEventListener("click", liedje12);
+nummer13.addEventListener("click", liedje13);
+nummer14.addEventListener("click", liedje14);
+nummer15.addEventListener("click", liedje15);
+nummer16.addEventListener("click", liedje16);
+nummer17.addEventListener("click", liedje17);
+nummer18.addEventListener("click", liedje18);
+nummer19.addEventListener("click", liedje19);
+nummer20.addEventListener("click", liedje20);
 
 
-
+// code met behulp van Sanne's Codepen Sorteert de code op alfabetisch goed en alfabetisch omgekeerd & https://listjs.com/
 var sort = document.querySelector(".sort1");
 var sort2 = document.querySelector(".sort2");
-var options = {
-  valueNames: [ 'name', 'tijd' ]
+var deOpties = {
+  valueNames: ['name', 'tijd']
 };
 
 // Stap 3.
-var charactersList = new List('theList', options);
+var charactersList = new List('theList', deOpties);
 
 // Stap 4.
-function sortLiedjes(){
-  charactersList.sort('name', { order: "asc" });
+var sortVolgordeNaam = 'asc';
+var sortVolgordeTijd = 'asc';
+
+function sortLiedjesNaam() {
+  if (sortVolgordeNaam === 'asc') {
+    charactersList.sort('name', {
+      order: 'asc'
+    }); // Alfabetische volgorde
+    sortVolgordeNaam = 'desc';  
+    sort.textContent = 'A-Z'; // geprobeerd dit te fixen in de CSS maar tervergeefs
+  } else {
+    charactersList.sort('name', {
+      order: 'desc'
+    }); // Omgekeerde alfabetische volgorde
+    sortVolgordeNaam = 'asc';
+    sort.textContent = 'Z-A'; // geprobeerd dit te fixen in de CSS maar tervergeefs
+  }
 }
 
-
-function sortLiedjes2(){
-  charactersList.sort('tijd', { order: "asc" });
+function sortLiedjesTijd() {
+  if (sortVolgordeTijd === 'asc') {
+    charactersList.sort('tijd', {
+      order: 'asc'
+    }); // Oplopende liedjes duratie
+    sortVolgordeTijd = 'desc';
+    sort2.textContent = 'Duratie ↑'; // geprobeerd dit te fixen in de CSS maar tervergeefs
+  } else {
+    charactersList.sort('tijd', {
+      order: 'desc'
+    }); // Aflopende liedjes duratie
+    sortVolgordeTijd = 'asc';
+    sort2.textContent = 'Duratie ↓'; // geprobeerd dit te fixen in de CSS maar tervergeefs
+  }
 }
 
-sort.addEventListener("click", sortLiedjes);
-sort2.addEventListener("click", sortLiedjes2);
+sort.addEventListener("click", sortLiedjesNaam);
+sort2.addEventListener("click", sortLiedjesTijd);
 
 
-
-// Drag & Drop
-
-// Stap 5. --> CSS vanaf regel 58
-
-/* hopla klaar */
-
-/* getting started --> http://sortablejs.github.io/Sortable/#simple-list */
-
-// Stap 1. --> klik op het tandwiel (vergeet de https:// niet)
-
+// Drag & Drop 
 var list = document.querySelector(".list");
-// Stap 2.
 new Sortable(list, {
   animation: 300, // ca 300ms is meestal een mooie tijd
 });
 
 
-// Stap 3. --> nu verder met de css
+// Dit zorgt ervoor dat wanneer er op een prullenbakje word gedrukt in een specifieke li dat de li word verwijderd
+var deleteButtons = document.querySelectorAll('#deleteButton');
+
+function deleteListItem() {
+  var listItem = this.closest('li');
+  if (listItem) {
+    listItem.remove();
+  }
+}
+
+deleteButtons.forEach(function (button) {
+  button.addEventListener('click', deleteListItem);
+});
 
 
-/* hopla klaar */
+
+// Shuffle knop
+
+var shuffleButton = document.getElementById("shuffle");  // Selecteer het shuffle-knop element met behulp van de ID
+var list = document.querySelector(".list");  // Selecteer het lijst-element met behulp van de class-naam
+
+// Functie voor het shuffelen van de lijst d.m.v math random
+function shuffleList() {
+  for (var i = list.children.length; i >= 0; i--) {
+    list.appendChild(list.children[Math.random() * i | 0]);  // Kies een willekeurig element uit de lijst en verplaats het naar het einde van de lijst
+  }
+}
+
+shuffleButton.addEventListener("click", shuffleList);  // Voeg een event listener toe aan de shuffle-knop
+
+
+
+
+// Omhoog en omlaag (met behulp van het internet)
+// bron: combinatie van https://www.w3schools.com/jsref/prop_element_previouselementsibling.asp en https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore
+
+const OmhoogKnop = document.querySelectorAll('.omhoog');
+const OmlaagKnop = document.querySelectorAll('.omlaag');
+
+function Omhoog() {
+  const listItem = document.activeElement.closest('li'); // Vind het dichtstbijzijnde ouder list item van het actieve element
+  const VorigItem = listItem.previousElementSibling; // Vind het vorige sibling element van het list item
+  if (VorigItem) {
+    listItem.parentNode.insertBefore(listItem, VorigItem); // Verplaats het list item voor het vorige sibling element
+  }
+}
+
+function Omlaag() {
+  const listItem = document.activeElement.closest('li'); // Vind het dichtstbijzijnde ouder list item van het actieve element
+  const VolgendItem = listItem.nextElementSibling; // Vind het volgende sibling element van het list item
+  if (VolgendItem) {
+    listItem.parentNode.insertBefore(VolgendItem, listItem); // Verplaats het volgende sibling element voor het list item
+  }
+}
+
+// het gebruiken van de arrow keys, de functie word aangeroepen als de keys worden ingedrukt op het toetsenbord
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowUp') {
+    Omhoog(); // Wanneer de arrow up key word ingedrukt dan word de functie omhoog opgeroepen
+  } else if (event.key === 'ArrowDown')  {
+    Omlaag(); // Wanneer de arrow down key word ingedrukt dan word de functie omlaag opgeroepen
+  }
+});
+
+OmhoogKnop.forEach(function (button) {
+  button.addEventListener('click', Omhoog);
+});
+
+OmlaagKnop.forEach(function (button) {
+  button.addEventListener('click', Omlaag); 
+});
+
+
